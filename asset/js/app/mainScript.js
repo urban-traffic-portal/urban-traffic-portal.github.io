5a13d27fb78a9b87b709001bc438555210e95e1a
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-03-26 02:49:38 +0700

    index

--- asset/js/app/mainScript.js
+++ asset/js/app/mainScript.js
@@ -1,5 +1,3 @@
-var guestName = "";
-
 function showAlert(title, message)
 {
   $("#alert .modal-title").html(title);
@@ -7,19 +5,21 @@ function showAlert(title, message)
   $("#alert").modal("show");
 }
 
-$(document).ready(function ()
+var guestName = "";
+var regName = /^[a-zA-Z ]{1,30}$/;
+$("#buttonGo").click(function ()
 {
-  var regName = /^[a-zA-Z ]{1,30}$/;
-  $("#buttonGo").click(function ()
+  guestName = $("#inputGuestName").val();
+  if (!regName.test(guestName))
+    showAlert("<span class=\"text-danger\"><i class=\"fas fa-exclamation-triangle\"></i> Alert</span>", "<b class=\"text-danger\">Invalid name</b>. Please check again.");
+  else
   {
-    var guestName = $("#inputGuestName").val();
-    if (!regName.test(guestName))
-    {
-      showAlert("<span class=\"text-danger\"><i class=\"fas fa-exclamation-triangle\"></i> Alert</span>", "<b class=\"text-danger\">Invalid name</b>. Please check again.");
-    }
-    else
-    {
-      alert("Dung");
-    }
-  });
+    $(".textGuestName").text(guestName);
+    $("#pageWelcome").fadeOut(function () { $("#pageMain").fadeIn(); });
+  }
 });
+$("#inputGuestName").keyup(function (event)
+{
+  if (event.keyCode === 13) $("#buttonGo").click();
+});
+
