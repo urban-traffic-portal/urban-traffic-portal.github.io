a010631200c6e569e673ec431c7694b55883bc8e
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-03-26 03:57:40 +0700

    fgfdgfdgfd

--- asset/js/app/mainRoute.js
+++ asset/js/app/mainRoute.js
@@ -1,39 +1,35 @@
-$(document).ready(function () {
-    $("#pageWelcome").fadeOut();
-});
+    var routerApp = angular.module('routerApp', ['ui.router']);
+    routerApp.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: './asset/views/map.html'
+            })
+            /*
+            // nested list with custom controller
+            .state('home.list', {
+                url: '/list',
+                templateUrl: 'partial-home-list.html',
+                controller: function ($scope) {
+                    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                }
+            })
+            // nested list with just some random string data
+            .state('home.paragraph', {
+                url: '/paragraph',
+                template: 'I could sure use a drink right now.'
+            })
+            */
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS
+            .state('about', {
+                url: '/about',
+                views: {
+                    // the main template will be placed here (relatively named)
+                    '': { templateUrl: './asset/views/about.html' },
+                    // the child views will be defined here (absolutely named)
+                    'columnOne@about': { template: 'Look I am a column!' },
+                }
+            });
 
-var routerApp = angular.module('routerApp', ['ui.router']);
-routerApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: './asset/views/home.html'
-        })
-        /*
-        // nested list with custom controller
-        .state('home.list', {
-            url: '/list',
-            templateUrl: 'partial-home-list.html',
-            controller: function ($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
-        })
-        // nested list with just some random string data
-        .state('home.paragraph', {
-            url: '/paragraph',
-            template: 'I could sure use a drink right now.'
-        })
-        */
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS
-        .state('about', {
-            url: '/about',
-            views: {
-                // the main template will be placed here (relatively named)
-                '': { templateUrl: './asset/views/about.html' },
-                // the child views will be defined here (absolutely named)
-                'columnOne@about': { template: 'Look I am a column!' },   
-            }
-        });
-
-}); // closes $routerApp.config()
+    }); // closes $routerApp.config()
