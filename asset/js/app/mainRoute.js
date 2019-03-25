c9860317bba5ad2698e9c2e695709bf26dc4ac7c
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-03-25 23:22:20 +0700

    update

--- asset/js/app/mainRoute.js
+++ asset/js/app/mainRoute.js
@@ -1,11 +1,16 @@
+$(document).ready(function () {
+    $("#pageWelcome").fadeOut();
+});
+
 var routerApp = angular.module('routerApp', ['ui.router']);
 routerApp.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: './views/home.html'
+            templateUrl: './asset/views/home.html'
         })
+        /*
         // nested list with custom controller
         .state('home.list', {
             url: '/list',
@@ -19,16 +24,16 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
             url: '/paragraph',
             template: 'I could sure use a drink right now.'
         })
+        */
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS
         .state('about', {
             url: '/about',
             views: {
                 // the main template will be placed here (relatively named)
-                '': { templateUrl: './views/about.html' },
+                '': { templateUrl: './asset/views/about.html' },
                 // the child views will be defined here (absolutely named)
                 'columnOne@about': { template: 'Look I am a column!' },   
             }
-
         });
 
 }); // closes $routerApp.config()
