65844e860f5f1fe38fe0c9218087bf70a3a395df
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-03-26 00:19:09 +0700

    update index

---
+++ asset/js/app/mainScript.js
@@ -0,0 +1,25 @@
+var guestName = "";
+
+function showAlert(title, message)
+{
+  $("#alert .modal-title").html(title);
+  $("#alert .modal-body").html(message);
+  $("#alert").modal("show");
+}
+
+$(document).ready(function ()
+{
+  var regName = /^[a-zA-Z ]{1,30}$/;
+  $("#buttonGo").click(function ()
+  {
+    var guestName = $("#inputGuestName").val();
+    if (!regName.test(guestName))
+    {
+      showAlert("<span class=\"text-danger\"><i class=\"fas fa-exclamation-triangle\"></i> Alert</span>", "<b class=\"text-danger\">Invalid name</b>, please check again.");
+    }
+    else
+    {
+      alert("Dung");
+    }
+  });
+});
