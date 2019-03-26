1a4e2c19f0a25e7a66687a55377d59c22bb25472
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-03-26 11:25:22 +0700

    index

--- asset/js/app/mainScript.js
+++ asset/js/app/mainScript.js
@@ -14,6 +14,14 @@ function updateViews()
   else $("#inputGuestName").attr("placeholder", "Type yourname here");
 }
 
+function reString(str)
+{
+  return str = str.replace(/ +(?= )/g, "").replace(/\w\S*/g, function (txt)
+  {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  }).trim();
+}
+
 updateViews();
 $(window).resize(function ()
 {
@@ -24,7 +32,7 @@ var guestName = "";
 var regName = /^[a-zA-Z ]{1,30}$/;
 $("#buttonGo").click(function ()
 {
-  guestName = $("#inputGuestName").val();
+  guestName = reString($("#inputGuestName").val());
   if (!regName.test(guestName))
     showAlert("<span class=\"text-danger\"><i class=\"fas fa-exclamation-triangle\"></i> Alert</span>", "<b class=\"text-danger\">Invalid name</b>. Please check again.");
   else
@@ -39,3 +47,50 @@ $("#inputGuestName").keyup(function (event)
   if (event.keyCode === 13) $("#buttonGo").click();
 });
 
+//----------------------------------------
+
+$("#sidebar").mCustomScrollbar({
+  theme: "minimal"
+});
+
+$("#dismiss, .overlay").on("click", function ()
+{
+  $("#sidebar").removeClass("active");
+  $(".overlay").removeClass("active");
+});
+
+$("#buttonSidebar").click(function ()
+{
+  $("#sidebar").addClass("active");
+
+  $(".overlay").addClass("active");
+  $(".collapse.in").toggleClass("in");
+  $("a[aria-expanded=true]").attr("aria-expanded", "false");
+});
+
+//----------------------------------------
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "No", "Dec"];
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function updateDate()
+{
+  var date = new Date();
+  var ampm = date.getHours() < 12 ? "AM" : "PM";
+  var hours = date.getHours() == 0 ? 12 : date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+  var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  var dayOfWeek = days[date.getDay()];
+  var month = months[date.getMonth()];
+  var day = date.getDate();
+  var year = date.getFullYear();
+  $("#widgetDate").text(dayOfWeek + ", " + month + " " + day + ", " + year);
+  $("#widgetTime").text(hours + " : " + minutes + " : " + seconds + " " + ampm);
+}
+updateDate();
+window.setInterval(updateDate, 1000);
+//-------------------------------------
+if (localStorage.views == null)
+  localStorage.views = Math.floor((Math.random() * 1000000) + 100000);
+else localStorage.views++;
+
+$("#widgetViews").html(localStorage.views + " <small>visited</small>");
+$("#widgetOnline").html(Math.floor((Math.random() * 10000) + 10000) + " <small>online</small>");
