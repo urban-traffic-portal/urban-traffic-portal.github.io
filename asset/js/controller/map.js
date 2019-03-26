5d32dd4b60ac230b60257a0966df05660dd92230
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-03-27 03:18:00 +0700

    tien doan

--- asset/js/controller/map.js
+++ asset/js/controller/map.js
@@ -11,6 +11,8 @@ function initMap(userlat, userlong, userzoom) {
   // The map, centered at Uluru
   var thisMap = new google.maps.Map(
   document.getElementById('map'), { zoom: userzoom, center: uluru ,mapTypeId: 'roadmap'});
+  var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(thisMap);
     //Autocmplete
     var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
@@ -92,6 +94,11 @@ function initMap(userlat, userlong, userzoom) {
     addMarker(event.latLng,thisMap);
   });
   // This event listener will call addMarker() when the map is clicked.
+
+  //TRAFFICE LAYER
+  //var trafficLayer = new google.maps.TrafficLayer();
+  //trafficLayer.setMap(thisMap);
+ 
   return mymap;
 }
 function getLocation() {
@@ -126,66 +133,4 @@ function Icons(name){
       icon: iconBase + 'info-i_maps.png'
     }
   };
-}
-
-///Auto compplete
-function initAutocomplete(userMap) {
-  var map = userMap;
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-  return map;
 }
