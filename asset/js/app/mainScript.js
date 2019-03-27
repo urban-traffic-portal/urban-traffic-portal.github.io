b6d3a29f094950afdd646172abafa8aedbfd8a7b
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-03-28 02:52:36 +0700

    Update mainScript.js

--- asset/js/app/mainScript.js
+++ asset/js/app/mainScript.js
@@ -127,7 +127,7 @@ var views = localStorage.getItem("views");
 if (views == null || views == "" || localStorage.views == undefined)
 {
   localStorage.setItem("views", Math.floor((Math.random() * 1000000) + 100000));
-  $("#widgetViews").html(views + " <small>visited</small>");
+  $("#widgetViews").html(localStorage.getItem("views") + " <small>visited</small>");
 }
 else
 {
